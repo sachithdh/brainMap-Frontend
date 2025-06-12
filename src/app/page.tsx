4454f395d1c67cad20aab58192f601c5668459e9
Nadun Madusanka <nadunmadusanka564@gmail.com>
2025-06-12 13:08:38 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Student from "./student/page";
+
+
+export default function Home() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Student/>
+    </>
+  );
+}
